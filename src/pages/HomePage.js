e6486d5d9476c41825d26da83f6c64672efd0f13
fatihcaliss <fthcls@gmail.com>
2023-02-05 00:34:26 +0300

    home page div md py margin added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,7 +13,7 @@ const HomePage = () => {
     }, [])
 
     return (
-        <div className="px-4 md:px-72 py-8 md:py-32 space-y-4 bg-white dark:bg-slate-900 text-gray-900 dark:text-gray-100">
+        <div className="px-4 md:px-72 py-32 md:py-32 space-y-4 bg-white dark:bg-slate-900 text-gray-900 dark:text-gray-100">
             <aside className="space-y-2">
                 <h2 className="text-4xl font-semibold">Top Artists List</h2>
             </aside>
